88f9036044879a7f3cee7b802d6b4f35c0e0cfb4
heychazza <ch@rli.es>
2022-01-11 10:32:51 +0000

    Added code

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+        './*.html',
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'castle': 'url(https://www.retailmerchantservices.co.uk/globalassets/card-machines/portable-card-machine-and-smartphone-making-a-contactless-payment.png)'
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
